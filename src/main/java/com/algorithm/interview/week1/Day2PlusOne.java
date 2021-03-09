f1e8dc9eb472f90bf8e2919d8e745d9312a970a0
cyx000cyx <472377823@qq.com>
2021-03-10 00:39:53 +0800

    Commit day 2 solution

---
+++ src/main/java/com/algorithm/interview/week1/Day2PlusOne.java
@@ -0,0 +1,41 @@
+package com.algorithm.interview.week1;
+
+import com.alibaba.fastjson.JSON;
+
+public class Day2PlusOne {
+    public int[] plusOne(int[] digits) {
+        boolean needAdd = true;
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] != 9) {
+                needAdd = false;
+            }
+        }
+        if (needAdd) {
+            int[] arr = new int[digits.length+1];
+            for (int i = 0; i < digits.length; i++) {
+                arr[i+1] = digits[i];
+            }
+            digits = arr;
+        }
+        int needPlus = 1;
+        for (int i = digits.length-1; i >= 0; i--) {
+            if (needPlus == 0) {
+                break;
+            }
+            if (digits[i] + needPlus > 9) {
+                digits[i] = 0;
+                needPlus = 1;
+            } else {
+                digits[i] += 1;
+                needPlus = 0;
+            }
+        }
+        return digits;
+    }
+
+    public static void main(String[] args) {
+        Day2PlusOne day = new Day2PlusOne();
+        int[] result = day.plusOne(new int[]{9, 9});
+        System.out.println(JSON.toJSONString(result));
+    }
+}
