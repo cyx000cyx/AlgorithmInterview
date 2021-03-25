273ba9cf8e5bc75c6eb115e3c37f890b25fc2cbf
cyx000cyx <472377823@qq.com>
2021-03-26 00:27:03 +0800

    Commit build tree util

--- src/main/java/com/algorithm/interview/common/BuildTreeUtils.java
+++ src/main/java/com/algorithm/interview/common/BuildTreeUtils.java
@@ -1,12 +1,23 @@
 package com.algorithm.interview.common;
 
+import com.alibaba.fastjson.JSON;
+
 public class BuildTreeUtils {
 
-    public TreeNode buildTree(int[] arr) {
-        return null;
+    public static TreeNode buildTreeByInorder(Integer[] arr, int pointer) {
+        if (pointer >= arr.length || arr[pointer] == null) {
+            return null;
+        }
+
+        TreeNode tn = new TreeNode();
+        tn.val = arr[pointer];
+        tn.left = buildTreeByInorder(arr, pointer * 2 + 1);
+        tn.right = buildTreeByInorder(arr, pointer * 2 + 2);
+        return tn;
     }
 
     public static void main(String[] args) {
-
+        TreeNode result = BuildTreeUtils.buildTreeByInorder(new Integer[]{3, 9, 20, null, null, 15, 7}, 0);
+        System.out.println(JSON.toJSONString(result));
     }
 }
