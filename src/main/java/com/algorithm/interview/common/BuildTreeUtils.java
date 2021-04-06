d9c7851ea71308b3fb6c99aa4d9d669fe18bb4ac
cyx000cyx <472377823@qq.com>
2021-04-07 00:06:36 +0800

    Commit Tree traversal

--- src/main/java/com/algorithm/interview/common/BuildTreeUtils.java
+++ src/main/java/com/algorithm/interview/common/BuildTreeUtils.java
@@ -2,6 +2,10 @@ package com.algorithm.interview.common;
 
 import com.alibaba.fastjson.JSON;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
 public class BuildTreeUtils {
 
     public static TreeNode buildTree(Integer[] arr, int pointer) {
@@ -16,13 +20,16 @@ public class BuildTreeUtils {
         return tn;
     }
 
-    public static void InorderTraversal(TreeNode root) {
+    public static void InorderTraversal(TreeNode root, Function<? super Object, ? extends Object> f) {
         if (root == null) {
             return;
         }
+        InorderTraversal(root.left, f);
+        f.apply(root.val);
+        InorderTraversal(root.right, f);
     }
 
-    public static void InorderTraversalByMorris(TreeNode root) {
+    public static void InorderTraversalByMorris(TreeNode root, Function<? super Object, ? extends Object> f) {
         if (root == null) {
             return;
         }
@@ -33,20 +40,56 @@ public class BuildTreeUtils {
         }
     }
 
-    public static void PostorderTraversal(TreeNode root) {
+    /**
+     * 后序遍历
+     * @param root
+     * @param f
+     */
+    public static void PostorderTraversal(TreeNode root, Function<? super Object, ? extends Object> f) {
         if (root == null) {
             return;
         }
+        PostorderTraversal(root.left, f);
+        PostorderTraversal(root.right, f);
+        f.apply(root.val);
     }
 
-    public static void PreorderTraversal(TreeNode root) {
+    /**
+     * 先序遍历
+     * @param root
+     * @param f
+     */
+    public static void PreorderTraversal(TreeNode root, Function<? super Object, ? extends Object> f) {
         if (root == null) {
             return;
         }
+        f.apply(root.val);
+        PreorderTraversal(root.left, f);
+        PreorderTraversal(root.right, f);
     }
 
     public static void main(String[] args) {
         TreeNode result = BuildTreeUtils.buildTree(new Integer[]{3, 9, 20, null, null, 15, 7}, 0);
         System.out.println(JSON.toJSONString(result));
+//        result = BuildTreeUtils.buildTree(new Integer[]{
+//                1,
+//                2,                              3,
+//                4, null,                        null, 5,
+//                null, 6, null, null,            null, null, null, null,
+//                null, null, 7, 8, null, null, null, null,   null, null, null, null, null, null, null, null}, 0);
+        BuildTreeUtils.InorderTraversal(result, a -> {
+            System.out.println(a);
+            return a;
+        });
+        System.out.println();
+        BuildTreeUtils.PreorderTraversal(result, a -> {
+            System.out.println(a);
+            return a;
+        });
+        System.out.println();
+        BuildTreeUtils.PostorderTraversal(result, a -> {
+            System.out.println(a);
+            return a;
+        });
     }
 }
