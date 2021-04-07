e53c31e19fa06421948a4817511a6a9413091cf9
cyx000cyx <472377823@qq.com>
2021-04-07 23:15:02 +0800

    Commit day 15 solution

--- src/main/java/com/algorithm/interview/week3/Day15InorderTraversal.java
+++ src/main/java/com/algorithm/interview/week3/Day15InorderTraversal.java
@@ -1,9 +1,33 @@
 package com.algorithm.interview.week3;
 
+import com.algorithm.interview.common.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
  */
 public class Day15InorderTraversal {
+
+    List<Integer> result = new ArrayList<Integer>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) {
+            return result;
+        }
+        inorderTraversalRecursive(root);
+        return result;
+    }
+
+    public void inorderTraversalRecursive(TreeNode r) {
+        if (r == null) {
+            return;
+        }
+        inorderTraversalRecursive(r.left);
+        result.add(r.val);
+        inorderTraversalRecursive(r.right);
+    }
 
     public static void main(String[] args) {
 
