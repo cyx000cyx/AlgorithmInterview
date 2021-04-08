be3959b631834fe02d11409e50b0b45d35fa013d
cyx000cyx <472377823@qq.com>
2021-04-09 00:10:03 +0800

    Commit day 30 solution

--- src/main/java/com/algorithm/interview/week5/Day30ReverseBits.java
+++ src/main/java/com/algorithm/interview/week5/Day30ReverseBits.java
@@ -1,15 +1,35 @@
 package com.algorithm.interview.week5;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * https://leetcode-cn.com/problems/reverse-bits/
  */
 public class Day30ReverseBits {
 
-    public int reverseBits(int n) {
-        return n;
+    public int reverseBits(long n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            if ((n % 2) == 1 || (n % 2) == -1) {
+                res += 1 << (31-i);
+            }
+            n >>= 1;
+        }
+        return res;
+    }
+
+    public void getBits(long n) {
+        for (int i = 0; i < 32; i++) {
+            System.out.print(Math.abs(n % 2));
+            n >>= 1;
+        }
     }
 
     public static void main(String[] args) {
-
+        Day30ReverseBits day = new Day30ReverseBits();
+        int res = day.reverseBits(((long) Integer.MAX_VALUE) * 2 - 1);
+        System.out.println(res);
+        System.out.println();
+        day.getBits((long)res);
     }
 }
